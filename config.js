3eeb72eca6901eb6c7051956ccd01623ae727c78
TITANLUX619 <26529507+TITANLUX619@users.noreply.github.com>
2019-02-05 16:56:51 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || "mongodb://" + MONGODB_SERVICE_SERVICE_HOST + ":27017/mongodb"
+  db: process.env.MONGODB || MONGODB_URI
 };
